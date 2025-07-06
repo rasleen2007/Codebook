a1ec39a8d3933b2048de0f0534d4e0206bca0e00
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-06 23:29:32 +0530

    Create Q5.java
    Q)To take name of the user as input and display it on the screen

---
+++ Q5.java
@@ -0,0 +1,13 @@
+//To take name of the user as input and display it on the screen
+import java.util.*;
+public class Input{
+        public static void main(String[] args){
+            //Input
+            Scanner sc= new Scanner(System.in);
+
+            String name=sc.nextLine();
+            System.out.println(name);
+            sc.close();
+        }
+}
+    
