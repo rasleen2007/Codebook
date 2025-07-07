17d28a1847de3defb6750d581e5242e0586479d6
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-07 22:40:38 +0530

    Q)To input two numbers "a", "b" and tell if they are equal,'a' is greater than 'b' or less than 'b'

---
+++ Comparetwonumbers.java
@@ -0,0 +1,20 @@
+//To input two numbers "a", "b" and tell if they are equal,'a' is greater than 'b' or less than 'b'
+import java.util.*;
+public class Q2{
+    public static void main(String[] args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        if(a==b){
+            System.out.println("Both values are equal");
+        }
+        else if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("a is less than b");
+        }
+        sc.close();
+        }
+    }
