ff59f9d969cc71a12b0f4d888bf90963d6c156e7
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-08 21:05:51 +0530

    To input a number from user and tell if it is odd or even

--- OddorEven.java
+++ OddorEven.java
@@ -1,6 +1,6 @@
 //To input a nuber from user and tell if it is odd or even 
 import java.util.*;
-public class Q3{
+public class OddorEven{
     public static void main(String[] args){
         //Input
         Scanner sc=new Scanner(System.in); 
