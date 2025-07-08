eb166b941f7313d8f9485fc29878df4596add9ff
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-08 20:57:44 +0530

    Use for loop to display hellow world 5 times

---
+++ Helloworldforloop.java
@@ -0,0 +1,8 @@
+//Use for loop to display hellow world 5 times 
+public class HelloWorldforloop{
+    public static void main(String[] args){
+          for(int counter=0;counter<5; counter=counter+1){
+            System.out.println("Hello world");
+          }
+    }
+}
