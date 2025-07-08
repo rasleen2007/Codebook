b8eff6a210763a11f8f90d4610793655e7d853a8
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-08 16:41:06 +0530

    Write a Java program that takes an integer input from the user (1, 2, or 3) and prints a greeting based on the number.

---
+++ Switch greeting button.java
@@ -0,0 +1,21 @@
+//Q: Write a Java program that takes an integer input from the user (1, 2, or 3) and prints a greeting based on the number.
+import java.util.*;
+public class Q2{
+    public static void main(String[] args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int Button= sc.nextInt();
+        
+        //applying switch
+        switch(Button){
+            case 1:System.out.println("Hello");
+            break;
+            case 2:System.out.println("Namaste");
+            break;
+            case 3:System.out.println("Bonjour");
+            break;
+            default:System.out.println("Invalid");
+        }
+        sc.close();
+    }
+}
