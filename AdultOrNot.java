1b50b91639c37ffeb9fafa081c8af793cfbe65c7
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-08 21:01:28 +0530

    To input the age from user and tell wether its an adult or not

--- AdultOrNot.java
+++ AdultOrNot.java
@@ -1,6 +1,6 @@
 //To input the age from user and tell wether its an adult or not
 import java.util.*; 
-public class Q2{
+public class AdultOrNot{
     public static void main(String[] args){
     //Input
     Scanner sc=new Scanner(System.in);
