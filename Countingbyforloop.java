352dd91ff40b8c209987b4b00eee6d7620641c51
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-09 18:24:18 +0530

    Using for loop print numbers from 0 to 10.

---
+++ Countingbyforloop.java
@@ -0,0 +1,9 @@
+//Using forloop print numbers from 0 to 10.
+public class Countingbyforloop{
+    public static void main(String[] args){
+        //for loop
+        for(int i=0;i<11;i=i+1){
+            System.out.println(i);
+        }
+    }
+}
