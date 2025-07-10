d4a3d54616139b1c547da808bce1acb6e9a7fe1a
rasleen2007 <83662684+rasleen2007@users.noreply.github.com>
2025-07-10 18:35:54 +0530

    Write a code to show that while loop checks the condition before running  the loop and do_while loop runs the run the loop and then check condition

---
+++ WhilevsDoWhileloop.java
@@ -0,0 +1,23 @@
+/*Write a code to show that while loop checks the condition before running
+ the loop and do_while loop runs the run the loop and then check condition*/
+public class Q2{
+    public static void main(String[] args){
+  
+        int i=12;
+        while(i<11){
+            System.out.println(i);
+            i++;}
+
+
+
+            int a=13;
+            do{
+                System.out.println(a);
+                i++;
+            }while(i<11);
+        
+            
+    }
+
+ }
+    
